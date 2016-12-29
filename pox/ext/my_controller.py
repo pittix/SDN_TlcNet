eff2ebc8bf678803b04fe4c13597704afc0c49b9
Matteo Maso <matteo.maso.1@studenti.unipd.it>
2016-12-29 06:46:55 -0800

    add controller e my_topo

---
+++ pox/ext/my_controller.py
@@ -0,0 +1,165 @@
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Controller
+by Matteo Maso & Andrea Pittaro
+"""
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from pox.lib.revent import *
+import pox.topology
+from pox.openflow.discovery import Discovery
+import pox.openflow.topology
+import pox.openflow.spanning_tree
+from pox.lib.util import dpid_to_str
+import pox.host_tracker
+from pox.lib.recoco import Timer  #for recoursive functions
+import pox.lib.packet as pkt
+from pox.lib.packet.ethernet import ethernet, ETHER_BROADCAST
+from pox.lib.packet.ipv4 import ipv4
+from pox.lib.packet.arp import arp
+from pox.lib.addresses import IPAddr, EthAddr
+from pox.lib.util import str_to_dpid
+import time
+import multiprocessing #multiprocess
+
+import my_topo_SDN as mt #new class
+
+
+topo = mt.topo() #our topology
+
+log = core.getLogger()
+
+def _handle_LinkEvent(event):
+    """
+    handle event ("LinkEvent") from openflow.discovery
+    """
+    l = event.link
+    if event.added:
+        log.debug('LinkAdd dpid1: {0} porta {1}, dpid2: {2} porta {3}'.format(l.dpid1, l.port1, l.dpid2, l.port2))
+        topo.add_link(l.dpid1, l.port1, l.dpid2, l.port2)
+    elif event.removed:
+        log.debug('LinkRemoved dpid1: {0} porta {1}, dpid2: {2} porta {3}'.format(l.dpid1, l.port1, l.dpid2, l.port2))
+        topo.rm_link(l.dpid1, l.port1, l.dpid2, l.port2)
+
+# def _handle_HostEvent(event):
+#     """
+#     handle HostEvent from discovery
+#     """
+#     mac_host = event.entry.macaddr
+#     dpid_sw = event.entry.dpid
+#     dpid_port = event.entry.port
+#     key1 = event.entry.ipAddrs.keys()
+#     log.debug("host event handler")
+#     # if event.join:
+#     #     if len(key1) > 0:
+#     #         host_ip = key1[0]
+#     #         topo.add_host(dpid_sw, mac_host, dpid_port, host_ip)
+#     #         log.debug("from host event %s", host_ip)
+#     #     else:
+#     #         pass #no ip i have only mac
+#     # elif event.leave:
+#     #     pass
+
+def _handle_ConnectionUp (event):
+    """
+    handle connection up from switch
+    """
+    topo.add_switch(event.connection.dpid)
+    log.debug("Add switch: %s", dpid_to_str(event.connection.dpid))
+
+def _handle_PacketIn(event):
+    packet = event.parsed       #this is the parsed packet data
+    src_mac = packet.src	    #mac del sorgente del pacchetto
+    dst_mac = packet.dst	    #mac del destinatario del pacchetto
+
+    if packet.type == packet.ARP_TYPE:
+
+        _handle_arp_packet(event)
+
+    elif packet.type == packet.IP_TYPE:
+
+        _handle_ip_packet(event)
+
+
+def _handle_arp_packet(event):
+    #log.debug("ARP_TYPE packet arrived")
+    packet = event.parsed
+
+    if packet.payload.opcode == arp.REQUEST:
+        ip_src = packet.payload.protosrc
+        ip_dst = packet.payload.protodst
+
+def _handle_ip_packet(event):
+    #log.debug("IP_TYPE packet arrived")
+    packet = event.parsed
+    src_mac = packet.src	    #mac del sorgente del pacchetto
+    dst_mac = packet.dst	    #mac del destinatario del pacchetto
+    ip_packet = packet.find('ipv4')
+
+    ip_src = ip_packet.srcip #ip sorgente
+    ip_dst = ip_packet.dstip #ip destinatario
+
+    #connessione alla rete
+    if ip_packet.dstip == IPAddr('10.10.0.0'):
+        topo.add_host(event.connection.dpid, src_mac, event.port, ip_packet.srcip)
+
+
+    log.debug("ip_src presente? %s" , topo.is_logged(ip_src))
+    log.debug("ip_dst presente? %s" , topo.is_logged(ip_dst))
+
+    #verifico se l'utente src e' gia' loggato nella rete
+    if topo.is_logged(ip_src):
+        log.debug("\n %s gia' presente nella rete", ip_src)
+    else:
+        topo.add_host(event.connection.dpid, src_mac, event.port, ip_packet.srcip)
+        log.debug("\n %s aggiunto nella rete", ip_src)
+
+    if topo.ip_connected(ip_src, ip_dst):
+        #installa le rotte di default con la minimum path
+        topo.add_default_path(ip_src, ip_dst)
+    else:
+        #i due host non sono connessi quindi o devo cercare il secondo
+        #trovare un modo, per ora possono comunicare solo se hanno gia' fatto un tentativo di accesso alla related
+        log.debug("path NON presente nel grafo")
+
+def _show_topo():
+    """
+    function to show the graph on a separate process
+    """
+    job_for_another_core = multiprocessing.Process(target=topo.save_graph,args=()) #chiama la funzione save_graph in un processo separato
+    job_for_another_core.start()
+
+def launch():
+    """
+    start:
+        pox.topology.launch()
+        pox.openflow.discovery
+        pox.openflow.topology
+        pox.openflow.spanning_tree
+
+    and make listeners functions
+    """
+    pox.topology.launch()
+    pox.openflow.discovery.launch()
+    pox.openflow.topology.launch()
+    pox.openflow.spanning_tree.launch()
+    #pox.host_tracker.launch()
+    core.openflow_discovery.addListenerByName("LinkEvent", _handle_LinkEvent)
+    #core.host_tracker.addListenerByName("HostEvent", _handle_HostEvent)
+    core.openflow.addListenerByName("PacketIn", _handle_PacketIn)
+    core.openflow.addListenerByName("ConnectionUp",_handle_ConnectionUp)
+
+
+    Timer(2, _show_topo, recurring=True) #every 2 seconds execute _show_topo
---
+++ pox/ext/my_topo_SDN.py
@@ -0,0 +1,192 @@
+#classe topo SDN
+#che utilizzo nel mio controller
+from pox.core import core
+from pox.lib.util import dpid_to_str
+import pox.openflow.libopenflow_01 as of
+from pox.lib.addresses import IPAddr, EthAddr
+
+import networkx as nx            #graph library
+import matplotlib.pyplot as plt  #for ploting graph
+
+log = core.getLogger()
+
+class topo():
+    def __init__(self):
+        self.switch = {} #dizionario di switch dpid e' la chiave
+        self.grafo = nx.Graph()
+        self.ip_to_switch = {} #dizionario in cui l'ip solo le chiavi e i valori gli elementi switch
+        self.mac_to_ip = {} #per gli host
+
+    def add_host(self, dpid, mac, port, ip):
+        """
+        add host on the graph and the port of switch
+        """
+        #devo aggiungere le porte allo switch
+        #da finire
+        x = True
+        try:
+            self.switch[dpid]
+        except:
+            log.warning("Add host to switch that don't exist")
+            x = False
+        if x: #se lo switch e' presente
+            self.switch[dpid].add_host(mac, port, ip)
+            self.mac_to_ip[mac] = ip
+            self.ip_to_switch[ip] = self.switch[dpid]
+            self.grafo.add_node(ip)
+            self.grafo.add_edge(dpid, ip)
+            log.debug("add host %s", ip)
+
+
+    def add_switch(self, dpid):
+        """
+        Add a switch if it wasn't already added
+        """
+        if self.switch.has_key(dpid):
+            pass #switch already added
+        else:
+            sw = my_Switch(dpid)
+            self.switch[dpid] = sw
+            self.grafo.add_node(dpid)
+            log.debug("Add switch: %s", dpid_to_str(dpid))
+            self.add_default_roules(dpid)
+
+    def rm_switch(self, dpid):
+        """
+        delete switch if it is present
+        """
+        x = True
+        try:
+            self.grafo.remove_node(dpid)
+            del self.switch[dpid]
+        except:
+            x = False
+        if x:
+            #elimina i link sui nodi che a lui erano collegati se possibile
+            pass
+
+    def save_graph(self):
+        nx.draw_networkx(self.grafo)  #stampa anche il grafo
+        plt.savefig("grafo.png")      #salva l'immagine
+        plt.clf()                     #elimina l'immagine corrente dalla libreria
+
+    def add_link(self, dpid1, port1, dpid2, port2):
+        """
+        Inside add_link function, add switch and link
+        """
+        self.add_switch(dpid1)
+        self.add_switch(dpid2)
+        self.switch[dpid1].port_dpid[port1] = dpid2
+        self.switch[dpid1].dpid_port[dpid2] = port1
+        self.switch[dpid2].port_dpid[port2] = dpid1
+        self.switch[dpid2].dpid_port[dpid1] = port2
+        self.grafo.add_edge(dpid1, dpid2)
+
+    def rm_link(self, dpid1, port1, dpid2, port2):
+        x = True
+        try:
+            self.grafo.remove_edge(dpid1, dpid2)
+        except:
+            #se tento di rimuovere un link che non e' presente nel grafo
+            x = False
+        if x:
+            del self.switch[dpid1].port_dpid[port1]
+            del self.switch[dpid1].dpid_port[dpid2]
+
+            del self.switch[dpid2].port_dpid[port2]
+            del self.switch[dpid2].dpid_port[dpid1]
+
+    def add_default_path(self, ip_src, ip_dst):
+        sw_list = nx.shortest_path(self.grafo,source=ip_src, target=ip_dst)
+        log.debug(sw_list) #show minimum path
+        for i in range (1, len(sw_list) - 2):
+            #installo i flussi da ip_src a ip_dst
+            msg = of.ofp_flow_mod()
+            msg.priority = 1001
+            msg.match.nw_dst = IPAddr(str(ip_dst))
+            msg.match.dl_type = 0x800 #ip
+            pt_next_hope = self.switch[sw_list[i]].dpid_port[sw_list[i+1]]
+            msg.actions.append(of.ofp_action_output(port = pt_next_hope ))
+            core.openflow.sendToDPID(sw_list[i], msg) #switch i-esimo
+
+            msg = of.ofp_flow_mod()
+            msg.priority = 150
+            msg.match.dl_type = 0x806 #arp reques
+            msg.match.nw_dst = IPAddr(str(ip_dst))
+            msg.actions.append(of.ofp_action_output(port = pt_next_hope ))
+            core.openflow.sendToDPID(sw_list[i], msg)
+
+        for i in range (2, len(sw_list) - 1):
+            #installo i flussi da ip_dst a ip_src
+            msg = of.ofp_flow_mod()
+            msg.priority = 1001
+            msg.match.nw_dst = IPAddr(str(ip_src))
+            msg.match.dl_type = 0x800 #ip
+            pt_pre_hope = self.switch[sw_list[i]].dpid_port[sw_list[i-1]]
+            msg.actions.append(of.ofp_action_output(port = pt_pre_hope ))
+            core.openflow.sendToDPID(sw_list[i], msg) #switch i-esimo
+
+            msg = of.ofp_flow_mod()
+            msg.priority = 150
+            msg.match.dl_type = 0x806 #arp reques
+            msg.match.nw_dst = IPAddr(str(ip_src))
+            msg.actions.append(of.ofp_action_output(port = pt_pre_hope ))
+            core.openflow.sendToDPID(sw_list[i], msg)
+
+    def add_default_roules(self, dpid):
+        """
+        add default roules on new switch
+        arp request flooding for now
+        """
+        msg = of.ofp_flow_mod()
+        msg.priority = 50
+        msg.match.dl_type = 0x806 #arp reques
+        msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD ))
+        core.openflow.sendToDPID(dpid, msg)
+
+    def ip_connected(self, ip1, ip2):
+        try:
+            nx.has_path(self.grafo, source=ip1, target=ip2)
+        except:
+            return False
+        return True
+
+    def is_logged(self, ip1):
+        try:
+            self.ip_to_switch[ip1]
+        except:
+            return False
+        return True
+
+class my_Switch():
+    def __init__(self, dpid):
+        self.dpid = dpid
+        self.dpid_port = {}  #chiave dpid/ip valore porta
+        self.port_dpid = {}
+        self.port_mac = {}
+        self.mac_port = {}
+
+    def add_host(self, mac, porta, ip):
+        "add host on the switch's port"
+        try:
+            pt = self.dpid_port[ip]
+            log.debug("IP still exist on the switch")
+        except:
+            self.dpid_port[ip] = porta
+            self.port_dpid[porta] = ip
+            self.port_mac[porta] = mac
+            self.mac_port[mac] = porta
+
+            msg = of.ofp_flow_mod()
+            msg.priority = 100
+            msg.match.dl_type = 0x806 #arp reques
+            msg.match.dl_dst = EthAddr(str(mac))
+            msg.actions.append(of.ofp_action_output(port = porta ))
+            core.openflow.sendToDPID(self.dpid, msg)
+
+            msg = of.ofp_flow_mod()
+            msg.priority = 1000
+            msg.match.nw_dst = IPAddr(str(ip))
+            msg.match.dl_type = 0x800 #ip
+            msg.actions.append(of.ofp_action_output(port = porta ))
+            core.openflow.sendToDPID(self.dpid, msg)
